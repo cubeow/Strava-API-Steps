aef8cf0f56a9f9e81badc29931fe92df01552e4d
Sage Wong <104601269+cubeow@users.noreply.github.com>
2024-09-12 16:40:05 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+auth_url = "https://www.strava.com/oauth/token"
+activites_url = "https://www.strava.com/api/v3/athlete/activities"
+
+payload = {
+    'client_id': "",
+    'client_secret': '',
+    'refresh_token': '',
+    'grant_type': "refresh_token",
+    'f': 'json'
+}
+
+print("Requesting Token...\n")
+res = requests.post(auth_url, data=payload, verify=False)
+access_token = res.json()['access_token']
+print("Access Token = {}\n".format(access_token))
+
+header = {'Authorization': 'Bearer ' + access_token}
+param = {'per_page': 200, 'page': 1}
+my_dataset = requests.get(activites_url, headers=header, params=param).json()
+
+with open("output.txt", "w", encoding="utf-8") as f:
+    f.write(str(my_dataset))
+f.close()
